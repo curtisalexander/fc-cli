950a2227927b4ad8dcff19aca6f161df8e4aa0f2
Curtis Alexander <calexander@elevate.com>
2016-03-07 11:06:06 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# fc_cli.py
+Command line script to query Full Contact's RESTful [Person API](https://www.fullcontact.com/developer/docs/person).  Able to query using
+* email
+* phone
+* Twitter username
+* Facebook username
+
+## Usage
+```
+fc.py [OPTIONS]
+
+Options:
+  --lookup [email|phone|twitter|facebook] lookup type
+  --lookup-value TEXT             lookup value: email, phone, twitter, facebook
+  --json PATH                     json file path
+  --help                          Show this message and exit.
+```
+
+## Example
+```
+fc.py --lookup=email --lookup-value=myaddress@gmail.com --json=results.json
+
+fc.py --lookup=phone --lookup-value=7737654321 --json=results.json
+```
+
+## Requirements
+Full Contact API key is required.  Place the key in the following location.
+
+`~/.fc_key`
+
+## Software Requirements
+* [click](http://click.pocoo.org/6) - Python package for creating beautiful command line interfaces 
+
+```
+pip install click
+```
+or
+```
+conda skeleton pypi click
+conda build click
+conda install --use-local click
+```
+* [requests](http://docs.python-requests.org/en/latest) - HTTP for Humans
+
+```
+pip install requests
+```
+or
+```
+conda install requests
+```
+
+## Related
+[fullcontact.py](https://github.com/garbados/fullcontact.py) - Python implmentation referenced by the Full Contact docs
---
+++ fc_cli.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# standard library
+import os
+import platform
+import re
+
+# third party
+import click
+import requests
+
+@click.command()
+@click.option('--lookup', type=click.Choice(['email', 'phone', 'twitter']), help='lookup type')
+@click.option('--lookup-value', help='lookup value: email, phone, twitter')
+@click.option('--json', type=click.Path(), help='json file path')
+
+# even though the cli option is 'lookup-value'
+# it is passed in as lookup_value
+def fc_cli(lookup, lookup_value, json):
+    """ Query the Full Contact Person API """
+    API_KEY = get_api_key('fc_key')
+    URL = 'https://api.fullcontact.com/v2/person.json'
+
+    headers = {'X-FullContact-APIKey': API_KEY}
+
+    parameters = {'prettyPrint': 'true'}
+    if lookup == 'email':
+        parameters['email'] = lookup_value
+    elif lookup == 'phone':
+        no_dash_paren = str.maketrans('', '', '()-')
+        parameters['phone'] = '+1' + lookup_value.translate(no_dash_paren)
+    elif lookup == 'twitter':
+        parameters['twitter'] = lookup_value
+    else:
+        raise ValueError('--lookup should be one of email, phone, or twitter')
+
+    r = requests.post(URL, headers=headers, params=parameters)
+    print('Full Contact return status code: {_status}'.format(_status = r.status_code))
+    print('Full Contact return datetime: {_dt}'.format(_dt = r.headers['Date']))
+    print('Full Contact rate limit: {_ratelimit} calls / {_ratelimitreset} seconds'.format( \
+        _ratelimit = r.headers['X-Rate-Limit-Limit'], \
+        _ratelimitreset = r.headers['X-Rate-Limit-Reset']))
+    print('Full Contact rate limit remaining: {_ratelimitremaining} calls / {_ratelimitreset} seconds'.format( \
+        _ratelimitremaining = r.headers['X-Rate-Limit-Remaining'], \
+        _ratelimitreset = r.headers['X-Rate-Limit-Reset']))
+
+    with open(json, 'w') as j:
+        j.write(r.text)
+
+def get_api_key(api_file):
+    """ Return an API key from the user's home directory """
+    with open('{_home}/.{_api_file}'.format(_home = os.path.expanduser('~'),
+                                            _api_file = api_file)) as f:
+        return f.read().replace('\n', '')
+
+if __name__ == '__main__':
+    fc_cli()
